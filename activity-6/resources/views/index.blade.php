6d226e2339bae69d92cf15943022a1089cc97991
emilio <emilio123456.cruz@gmail.com>
2024-03-16 11:58:30 -0600

    Agregado index.blade.php

---
+++ activity-6/resources/views/index.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index Page</title>
+</head>
+<body>
+    <h1>Bienvenido a la página de índice</h1>
+    <p>Esta es la página de inicio de tu aplicación Laravel.</p>
+
+    <!-- Agregar botón "Agregar Producto" -->
+    <a href="{{ route('create') }}">Agregar Producto</a>
+</body>
+</html>
